1df6ac786e31bf699a5dd09e35f00559140400ac
penghuailiang <penghuailiang@126.com>
2018-09-14 09:52:43 +0800

    protobuf

--- Assets/uLua/Examples/08_ProtoBuffer/LuaProtoBuffer01.cs
+++ Assets/uLua/Examples/08_ProtoBuffer/LuaProtoBuffer01.cs
@@ -1,19 +1,17 @@
-﻿using UnityEngine;
-using System.Collections;
-using LuaInterface;
-using System;
+﻿using LuaInterface;
+using UnityEngine;
 
 public class LuaProtoBuffer01 : MonoBehaviour
 {
     private string script = @"      
         function decoder()  
-            local msg = person_pb.Person()
+            local msg = login_pb.LoginRequest()
             msg:ParseFromString(TestProtol.data)
             print('id:'..msg.id..' name:'..msg.name..' email:'..msg.email)
         end
 
         function encoder()                           
-            local msg = person_pb.Person()
+            local msg = login_pb.LoginRequest()
             msg.id = 100
             msg.name = 'foo'
             msg.email = 'bar'
@@ -22,14 +20,12 @@ public class LuaProtoBuffer01 : MonoBehaviour
         end
         ";
 
-    //实际应用如Socket.Send(LuaStringBuffer lsb)函数（功能发送lsb.buffer） , 在lua中调用Socket.Send(pb_data)
-    //读取协议 Socket.PeekMsgPacket() {return MsgPacket}; lua 中，取协议字节流 MsgPacket.data
     void Start()
     {
         LuaScriptMgr mgr = new LuaScriptMgr();
         mgr.Start();
         TestProtolWrap.Register(mgr.GetL());
-        mgr.DoFile("3rd/pblua/person_pb.lua");
+        mgr.DoFile("3rd/pblua/login_pb");
         mgr.DoString(script);
 
         LuaFunction func = mgr.GetLuaFunction("encoder");
