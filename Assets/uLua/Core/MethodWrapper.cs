82732e5a6a8890945e568e669c8a19c1a60edafb
penghuailiang <penghuailiang@126.com>
2018-09-20 15:52:32 +0800

    optimus

--- Assets/uLua/Core/MethodWrapper.cs
+++ Assets/uLua/Core/MethodWrapper.cs
@@ -417,8 +417,6 @@
          */
         public Delegate Add(LuaFunction function)
         {
-            //CP: Fix by Ben Bryant for event handling with one parameter
-            //link: http://luaforge.net/forum/message.php?msg_id=9266
             Delegate handlerDelegate = CodeGeneration.Instance.GetDelegate(eventInfo.EventHandlerType, function);
             eventInfo.AddEventHandler(target, handlerDelegate);
             pendingEvents.Add(handlerDelegate, this);
