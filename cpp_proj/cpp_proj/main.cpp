edc19103bec9316fb8fb95ae7cc7a57ea4b5416c
penghuailiang <penghuailiang@126.com>
2019-11-19 17:22:53 +0800

    lua unuse

--- cpp_proj/cpp_proj/main.cpp
+++ cpp_proj/cpp_proj/main.cpp
@@ -1,22 +1,16 @@
 #include "xlua.h"
-#include "xstate.h"
-#include "ulua.h"
 #include "xtable.h"
 #include "behit.h"
 
 using namespace std;
 
-xstate* pState;
 xlua* pXlua;
-ulua* pUlua;
 xtable* pTable;
 Behit* pTab;
 
 void init()
 {
-	pState = new xstate();
 	pXlua = new xlua();
-	pUlua = new ulua();
 	string *title = new string[3]{ "c1", "c2","c3" };
 	pTable = new xtable("m_table", 4, 3, title);
 	pTab = new Behit("BeHit.bytes");
@@ -24,15 +18,13 @@ void init()
 
 void tip()
 {
-	cout << "** 1.state, 2.xlua, 3.ulua, 4.table, any other number break **" << endl;
+	cout << "** 1.xlua, 2.table, 3.behit, any other number break **" << endl;
 	cout << "please your input op: ";
 }
 
 void dispose()
 {
-	SAFE_DELETE(pState);
 	SAFE_DELETE(pXlua);
-	SAFE_DELETE(pUlua);
 	SAFE_DELETE(pTable);
 	SAFE_DELETE(pTab);
 }
@@ -41,27 +33,19 @@ int main()
 {
 	cout << "32 int: " << sizeof(int32_t) << endl;
 	init();
-	int a = 0;
+	int cmd = 0;
 	bool loop = true;
 	while (loop)
 	{
 		tip();
-		cin >> a;
-		switch (a)
+		cin >> cmd;
+		switch (cmd)
 		{
 		case 1:
-			pState->exec();
-			break;
-		case 2:
 			pXlua->exec();
-			break;
-		case 3:
-			pUlua->exec();
-			break;
-		case 4:
+		case 2:
 			pTable->exec();
-			break;
-		case 5:
+		case 3:
 			pTab->Read();
 		default:
 			loop = false;
